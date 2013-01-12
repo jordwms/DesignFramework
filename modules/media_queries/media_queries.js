8a5d3594d629e4933699594e41d553631a4b9746
Morgan Jeffrey <taraja@gmail.com>
2013-01-11 17:20:01 -0700

    Fixed media queries js

--- modules/media_queries/media_queries.js
+++ modules/media_queries/media_queries.js
@@ -1,15 +1,11 @@
-<script type="text/javascript">
+$(document).ready(function(){
 
-    $(document).ready(function(){
+  //this shows the current screen width in px;
+  $('#current_screen_width').text( $(document).width() );
 
-      //this shows the current screen width in px;
-      $('#current_screen_width').text( $(document).width() );
+  //this updates the #current_screen_width when the window is resized
+  $(window).bind('resize', function () {
+    $('#current_screen_width').text( $(document).width() );
+  });
 
-      //this updates the #current_screen_width when the window is resized
-      $(window).bind('resize', function () {
-        $('#current_screen_width').text( $(document).width() );
-      });
-
-    });
-
-</script>
+});
