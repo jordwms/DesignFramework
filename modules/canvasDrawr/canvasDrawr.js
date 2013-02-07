658bd64cb85f19832bfe87e55d3d21766cd9871b
Jordan <profjord@gmail.com>
2013-02-07 15:15:14 -0700

    Renamed draw_area to canvasDrawr for consistency/sanity. Added preloaded image, too.

--- modules/canvasDrawr/canvasDrawr.js
+++ modules/canvasDrawr/canvasDrawr.js
@@ -1,35 +1,67 @@
-           // canvasDrawr originally from Mike Taylr  http://miketaylr.com/
-           // Tim Branyen massaged it: http://timbranyen.com/
-           // and i did too. with multi touch.
-           // and boris fixed some touch identifier stuff to be more specific.
-           
-var CanvasDrawr = function(options) {
-    // grab canvas element
-    var canvas = $('canvas#'+options.id)[0], // Retrieving the DOM element from the jQ object
+/*
+* This script performs several functions with joint purposes related to painting on a canvas.
+* 1. Draw on a canvas
+* 2. Maintain an image on the canvas when canvas resizes
+* 3. Draw an pre-determined image to the canvas
+* 4. Clear the canvas
+* 5. Saves every time the user stops drawing
+*
+* In order to provide all these abilities, it is expected that the "id" parameter is used across
+*   several HTML tags as a class or id attribute identifier.
+* The following elements are expected to be on the HTML document:
+* 1. <canvas> (class attribute)
+* 2. <input type="button"> OR <button> (class)
+* 3. <input type="hidden"> (name)
+*
+* "name" is used on the hidden field for form submission in the POST array.
+* "hidden" type input should hold a base64 encoded string for an
+*   image, like toDataURL( ) would generate
+*/
+var canvasDrawr = function(options) {
+    // Grab canvas element
+    var canvas = $('canvas.'+options.name)[0],
         ctxt = canvas.getContext("2d");
 
     ctxt.pX = undefined;
     ctxt.pY = undefined;
 
-    var is_move = false; // Tracks whether or not the user is click-and-dragging
-    var mouse_id = 0; // This is the only way to keep track of mouse actions
+    // Tracks whether or not the user is click-and-dragging
+    var is_move = false;
+    // This is the only way to keep track of mouse actions
+    var mouse_id = 0;
     var lines = [];
     var offset;
     
     var self = {
-        //bind click events
         init: function() {
-            self.set();
+            // These 3 lines keep the canvas from 'zooming', which ensures the line follows exactly where the points indicate
+            canvas.style.width = '100%';
+            canvas.width = canvas.offsetWidth;
 
-            // Reset the canvas
-            $('button#'+options.id).click(function() {
+            ctxt.lineWidth = options.size;
+            ctxt.lineCap = options.lineCap || "round";
+
+            offset = $(canvas).offset();
+
+            // Reset the canvas - the selector reacts to <input type="button" /> or <button>
+            $('button.'+options.name+', input[type=button].'+options.name).click(function() {
                 ctxt.setTransform(1, 0, 0, 1, 0, 0);
                 ctxt.clearRect(0, 0, canvas.width, canvas.height);
+
+                 $('input[name="'+options.name+'"]').val('');
+
             });
 
-            $(window).resize(function() {
-                // self.set();
+            // Load signature if image is found in hidden field
+            var img = new Image();
+            img.src = $('input[name="'+options.name+'"]').val();
+            
+            img.onload = function() {
+                ctxt.drawImage(img, 0, 0);
+            };
 
+            // Keep the current image on the canvas when resizing
+            $(window).resize(function() {
                 // Create a temporary canvas obj to cache the pixel data
                 var temp_cnvs = document.createElement('canvas');
                 var temp_cntx = temp_cnvs.getContext('2d');
@@ -51,37 +83,26 @@ var CanvasDrawr = function(options) {
                 offset = $(canvas).offset();
             });
             
-            //set pX and pY from first click
-            
+            // Bind click events
             canvas.addEventListener('touchstart', self.preMove, false);
             canvas.addEventListener('touchmove', self.move, false);
+            canvas.addEventListener('touchend', self.save, false);
 
             canvas.addEventListener('mousedown', self.mouse_startMove, false);
             canvas.addEventListener('mousemove', self.mouse_move, false);
-            window.addEventListener('mouseup',   self.mouse_endMove, false); // window so the line won't draw when mouse is off canvas
+            window.addEventListener('mouseup',   self.mouse_endMove, false);
             
-        },
-
-        set: function() {
-
-            // These 3 lines keep the canvas from 'zooming', which ensures the line follows exactly where the points indicate
-            canvas.style.width = '100%';
-            canvas.width = canvas.offsetWidth;
-
-            ctxt.lineWidth = options.size;
-            ctxt.lineCap = options.lineCap || "round";
-
-
-            offset = $(canvas).offset(); // I don't ~think~ this needs canvas.width= canvas.offsetWidth; making note of it here...
         },
 
         preMove: function(event) {
 
-            $.each(event.touches, function(i, touch) { // Each time the canvas is touched...
-              
-                var id      = touch.identifier; // Give the touch event an identifier
-              
-                lines[id] = { x     : this.pageX - offset.left, // Determine it's position on the canvas
+            // Each time the canvas is touched...
+            $.each(event.touches, function(i, touch) {
+                // Give the touch event an identifier
+                var id      = touch.identifier;
+                
+                // Determine event's position on the canvas
+                lines[id] = { x     : this.pageX - offset.left,
                               y     : this.pageY - offset.top
                             };
             });
@@ -92,8 +113,9 @@ var CanvasDrawr = function(options) {
         move: function(event) {
 
             $.each(event.touches, function(i, touch) {
+                // Dynamically find the position of X,Y
                 var id = touch.identifier,
-                    moveX = this.pageX - offset.left - lines[id].x, // Dynamically find the position of X,Y
+                    moveX = this.pageX - offset.left - lines[id].x,
                     moveY = this.pageY - offset.top - lines[id].y;
 
                 var ret = self.draw(id, moveX, moveY);
@@ -120,11 +142,17 @@ var CanvasDrawr = function(options) {
 
             return { x: lines[i].x + changeX, y: lines[i].y + changeY };
         },
+        
+        // Write base 64 image to a hidden element so the image can be passed to PHP
+        save: function(event) {
+            $('input[name="'+options.name+'"]').val( canvas.toDataURL() );
+        },
 
         mouse_startMove: function(event) {
 
+            // Position on the canvas found as coords on the page minus the offset of the canvas
             lines[mouse_id] = {
-                x     : event.pageX - offset.left, // Position on the canvas found as coords on the page minus the offset of the canvas
+                x     : event.pageX - offset.left,
                 y     : event.pageY - offset.top
             };
             is_move = true;
@@ -134,6 +162,7 @@ var CanvasDrawr = function(options) {
 
         mouse_endMove: function(event) {
             is_move = false;
+            self.save(event);
             event.preventDefault();
         },
 
