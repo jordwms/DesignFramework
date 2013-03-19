353ce4c7b4afe56c6f2728a99e2457d6d0017615
Morgan Jeffrey <taraja@gmail.com>
2013-03-19 13:10:11 -0600

    Created js friendly and unfriendly version of test_form with flexbox-like effect.

--- modules/test_form/test_form.js
+++ modules/test_form/test_form.js
@@ -13,4 +13,69 @@ $(document).ready(function() {
         $(this).closest("div").removeClass('active');
     });
 
+    /*
+     *  Preceding sibling calls for flex-box effect
+     */
+
 });
+
+$(document).ready(flex);
+$(window).resize(flex);
+
+function flex() {
+    var width = $(window).width();
+    $('span.input').css('width', '');
+    $('span.action').css('width', '');
+
+    if (width >= 0 && width < 480) {
+        $('span.input').css('width', '96%');
+
+        $('span.action').prevAll('span.input').css('width','82%');
+    }
+    else {
+        if (width >= 480 && width < 800) {
+            $('span.input').css('width', '97%');
+
+            $('span.unit').prevAll('span.input').css('width','83%');
+
+            $('span.action').prevAll('span.input').css('width','78%');
+
+            $('span.input').next('span.action').css('width','13%');
+
+            $('span.action').prev('span.input').css('width','87%');
+        }
+        else {
+            if (width >= 800 && width < 1024) {
+                $('span.input').css('width', '98%');
+
+                $('span.unit').prev('span.input').css('width', '90%');
+
+                $('span.info').prev('span.input').css('width', '50%');
+
+                $('span.input').next('span.action').css('width','7%');
+
+                $('span.action').prev('span.input').css('width', '93%');
+
+                $('span.info').prev().prev('span.input').css('width', '50%')
+            }
+            else {
+                if (width >= 1024) {
+                    $('span.input').css('width', '98%');
+
+                    $('span.unit').prev('span.input').css('width', '90%');
+
+                    $('span.info').prev('span.input').css('width', '50%');
+
+                    $('span.input').next('span.action').css('width','7%');
+
+                    $('span.action').prev('span.input').css('width', '93%');
+
+                    $('span.info').prev().prev('span.input').css('width', '50%')
+                }
+                else {
+                    
+                }
+            }
+        }
+    }
+};
