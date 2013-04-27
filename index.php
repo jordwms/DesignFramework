5654d03675622f76ba2e49661b126b3f9d35afa9
rm <robert@breeze.local>
2013-04-27 14:11:18 -0600

    better modules - now loads all .js and .css files in the module directory
    module html can now be called index.html or module_name.html
    Added/rewrote lots of comments

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 	/*
-	 *	Modular HTML/CSS/JS Loader with shared global resources
+	 *	Dynamic HTML/CSS/JS Loader with shared global resources
 	 *	for things like header/footer and css/js libraries that
 	 *  will be used across all modules.
+	 *
+	 *	We use a layout paradigm for the global html header/footer "template"
+	 *
+	 *	A module consists of a folder which conrains an index.html (or module_name.html) 
+	 *  and any .css and .js files in the same direcotry
+	 *  The folder name defines the module name
+	 *
+	 *  1. get paths to global css files (vendor libraries)
+	 *  2. start building the $css string with the html tags to inject into the template
+	 *  3. build the $js string by doing the same for global javascript resources
+	 *  4. get the module html or build a list of modules
+	 *  5. get the global layout which will inject the above mentioned strings
 	 */
 
 	//script execution timing
@@ -68,28 +80,70 @@
 		}
 	});
 
-	//Build the script html tags
+	//Build the $js string with html script tags
 	$js = array_map(
 		function($js_file){
-			return '<script src="'.MODULES_DIR.'global/js/'.$js_file.'"></script>';
+			return '<script src="'.GLOBAL_DIR.'js/'.$js_file.'"></script>';
 		},
 		$js_includes
 	);
 	$js = implode(NL, $js);
 
 	/*
-	 *	Component Modules
+	 *	Modules
+	 *
 	 *  Begin by building a list of available modules
 	 *  A module is simply a sub directory in the modules folder
 	 *  For obvious reasons, the name "global" is reserved and thus not included in the list
 	 */
-	$modules = array_diff(scandir(MODULES_DIR), array('..', '.', 'global'));
+	$modules = array_diff(scandir(MODULES_DIR), array('..', '.', '.DS_Store', 'global'));
 
 	/*
-	 *	Simple Router, either we show a list of available modules
-	 *  or load the requested module if it exists
+	 *	First see if the module exists and load it
+	 *  If not, show a list of modules
 	 */
-	if( count($_GET) === 0 ){
+	if( count($_GET) > 0 ){
+		/*
+		 *	This verifies the modules exists and loads it's resources
+		 */
+		$module_name = key($_GET);
+		if( in_array($module_name, $modules) ){
+			$module_path = MODULES_DIR.$module_name.'/';
+
+			//load either index.html or module_name.html
+			$module_html = $module_path.'index.html';
+			if( ! file_exists($module_html) )
+				$module_html = $module_path.$module_name.'.html';
+
+			//if neither file exists... fail 404
+			if( ! file_exists($module_html) ){
+				header("HTTP/1.0 404 Not Found");
+				require(GLOBAL_DIR.'html/404.html');
+				die;
+			}
+				
+			$module_files = array_diff(scandir(MODULES_DIR.$module_name.'/'), array('..', '.'));
+			foreach ($module_files as $file_name) {
+				if( strlen($file_name) - strpos($file_name, '.css') == 4 ){
+					$css .= '<link rel="stylesheet" href="'.$module_path.$file_name.'">';
+				}elseif( strlen($file_name) - strpos($file_name, '.js') == 3 ){
+					$js .= NL.'<script src="'.$module_path.$file_name.'"></script>';
+				}
+			}
+
+			ob_start();
+			require($module_html);
+			$html = ob_get_clean();
+		} else {
+			header("HTTP/1.0 404 Not Found");
+			require(GLOBAL_DIR.'html/404.html');
+			die;
+		}
+	} else {
+		/*
+		 * This is where we build the list of modules
+		 */
+
 		//show list of modules :D
 		$module_list = array_map(
 			function($module){
@@ -97,33 +151,15 @@
 			},
 			$modules
 		);
-	}else{
-		//echo '<header>'.key($_GET).'</header>';
-		if( in_array(key($_GET), $modules) ){
-			$module_html = MODULES_DIR.key($_GET).'/'.key($_GET).'.html';
-			$css .= '<link rel="stylesheet" href="'.MODULES_DIR.key($_GET).'/'.key($_GET).'.css">';
-			$js .= NL.'<script src="'.MODULES_DIR.key($_GET).'/'.key($_GET).'.js"></script>';
-		} else {
-			header("HTTP/1.0 404 Not Found");
-			require(GLOBAL_DIR.'html/404.html');
-		}
-	}
 
-	//Start building the html output, begenning with the header
-	if( file_exists(GLOBAL_DIR.'html/header.html') ){ 
-		require(GLOBAL_DIR.'html/header.html'); 
-	}
-
-	if( isset($module_list) ){
-		echo implode('', $module_list);
-	}
-	if( isset($module_html) ){
-		require($module_html);
+		$html = implode('', $module_list);
 	}
 
 	$script_execution_end = microtime(true);
 	$execution_time = $script_execution_end - $script_execution_start;
 
-	if( file_exists(GLOBAL_DIR.'html/footer.html') ){ 
-		require(GLOBAL_DIR.'html/footer.html');
-	}
+	// finally get they layout
+	// the layout has access to all variables here
+	// $html, $js, $css are the primary variables whcih contain the markup tags necessary to load all of the resources
+	// $execution_time can be handy for performance benchmarking
+	require(GLOBAL_DIR.'html/layout.html');
